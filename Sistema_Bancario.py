44063796818793481c5793b5091b954caa9dbeca
ldinizbr <ldinizbr@yahoo.com.br>
2024-10-02 20:47:10 -0300

    Carregar código final

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -8,24 +8,118 @@
 
 # Sistema Bancario V1.0
 
-saldo=0
-valor_deposito=0
-valor_saque=0
+# Importante função
+from datetime import datetime
 
-def deposito(saldo,valor_deposito):
-    valor_deposito=print("Digite o valor a ser depositado")
-    return saldo += valor_deposito
-
-def saque(saldo,valor_saque):
-    valor_saque=print("Digite o valor que deseja sacar")
-    if valor_saque>500:
-        valor_saque=print("Valor de saque acima do limite, digite valor até R$500,00"end="\n")
-    elif valor_saque>saldo:
-        valor_saque=print(f"Você não tem saldo suficiente, digite valor até R${saldo}"end="\n")
-        
+# Definindo a função saque ------------------------------------------------------------------------
+def saque(saldo,limite_saque):
+    
+    if valor_saque>limite_saque:
+        mensagem1="Valor acima do limite permitido. Seu limite de cada saque é " + str(limite_saque)
     else:
+        mensagem1=""
+        
+    if valor_saque>saldo:
+        mensagem2="Saldo insuficiente. Seu saldo atual é " + str(saldo)
+    else:
+        mensagem2=""
+        
+    if mensagem1=="" and mensagem2=="":
+        saldo -=valor_saque
         print("Saque realizado com sucesso")
-        return saldo-=valor_saque
+        data_hora=datetime.now()
+        data_hora=data_hora.strftime("%d/%m/%Y %H:%M:%S")
+        opcao=""
+    else:
+        print(f"{mensagem1}\n{mensagem2}")
+        data_hora=""
+        opcao=2
+    
+    return saldo,data_hora,opcao
+
+# Definindo a função depósito ---------------------------------------------------------------------
+def deposito(saldo,valor_deposito):
+    
+    
+    if valor_deposito<=0:
+        print("Valores 'zero' ou negativos não são válidos")
+        data_hora=""
+        opcao=1
+    else:
+        saldo+=valor_deposito
+        print("Depósito realizado com sucesso")
+        data_hora=datetime.now()
+        data_hora=data_hora.strftime("%d/%m/%Y %H:%M:%S")
+        opcao=""   
+        
+    return saldo,data_hora,opcao
+
+# Iniciando as variáveis
+
+menu_inicial=  """
+    ============= MENU =============
+
+    1 - Depositar
+    2 - Sacar
+    3 - Extrato
+    0 - Sair
+
+    ================================
+
+            Obrigado por usar nosso sistema!!!!
+"""
+opcao=""
+
+saldo=float(400)
+data_hora=0
+valor_deposito=float(0)
+valor_saque=float(0)
+limite_saque=float(500)
+valor=0
+data_hora=0
+extrato=[["Tipo","Valor","Data/horário","Saldo Atual"]]
+numero_saque=0
+
+# Programa Bancário. Criando o programa principal ---------------------------------------
+
+# Criando while para rodar programa de forma contínua.
+while opcao!=0:
+
+    if opcao=="":# if01 Verificicar se estamos iniciando a operação
+        print(menu_inicial)
+        opcao=int(input(":"))
+    else:# if01
+        if opcao==1: # if02 Verfica se o usuário selecionou Depositar
+            valor_deposito=float(input("Digite o valor que deseja depositar: "))
+            saldo,data_hora,opcao=deposito(saldo,valor_deposito)
+            if opcao=="":
+                extrato.append(["Depósito",valor_deposito,data_hora,saldo])
+                            
+        elif opcao==2:# Opção de saque
+            if numero_saque>=3:# verifica a quantidade de de saques
+                print("Você já realizou os 3 saques do seu limite diário.\nEscolha outra opção")
+                opcao=""
+            else:
+                valor_saque=float(input("Digite o valor que deseja sacar: "))
+                saldo,data_hora,opcao=saque(saldo,limite_saque)
+                if opcao=="":
+                    extrato.append(["Saque",valor_saque,data_hora,saldo])
+                    numero_saque+=1
+            
+        elif opcao==3: # Opção de extrato
+            if len(extrato)==1:
+                print("Não foram realizadas movimentações")
+            else:
+               for linha in extrato:
+                    print(linha)
+               opcao=""
+
+        elif opcao==0:# Deseja sair do programa
+            break
+        
+        else:# if02 Nenhuma opção válida foi digitada. Solicitando que digite novamente.
+            print("Opção inválida. Digite novamente")
+            opcao=int(input(":"))
 
 
 
